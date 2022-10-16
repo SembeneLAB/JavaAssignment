be10080bafe23831b37b973fb90f5da0876c9f5e
Mandela <elhadjndoye276@gmail.com>
2022-10-16 13:03:59 +0000

    second commit

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,11 +1,46 @@
-import javax.xml.stream.events.Comment;
-
 public class TryingMethods {
   /* This class try a method*/
 
   public static void main(String[] args) {
     
   }
+  String firstdynamicmethod(Integer number) {
+    switch (number) {
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;
+    
+      default:
+        break;
+    }
+    return null;
 
+     
+
+  }
   
+  public static void secondstaticmethod() {
+    
+  }
+  public void seconddynamicmethod() {
+    
+  }
 }
